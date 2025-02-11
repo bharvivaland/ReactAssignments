4529bcc81c1e3c1b389d4cf852fbc4978c54c16b
bharvivaland <bharvimvaland@gmail.com>
2025-02-11 11:59:20 +0530

    2303031087002-Assignment32

---
+++ src/assets/Greet.jsx
@@ -0,0 +1,16 @@
+// Greet.jsx
+
+import React from "react";
+
+// Function Declaration
+function Greet() {
+  return <h1>Hello, Bharvi Valand!</h1>;
+}
+
+// Arrow Function
+const GreetArrow = () => {
+  return <h1>Hello, Welcome to ReactJs! (Arrow Function)</h1>;
+};
+
+// Export both components
+export { Greet, GreetArrow };
