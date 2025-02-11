9355cedcf1bace8a87a4a230f0d6e37ec9c1e23f
bharvivaland <bharvimvaland@gmail.com>
2025-02-11 14:39:04 +0530

    2303031087002-Assignment33

---
+++ task3/Task3/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+    return (
+      <footer>
+        <p>© 2025 Bharvi Valand</p>
+      </footer>
+    );
+  }
+  
+  export default Footer;
+  
---
+++ task3/Task3/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+function MainContent() {
+    return (
+      <main>
+        <h2>Main Content</h2>
+        <p>This is some content about my portfolio and projects.</p>
+      </main>
+    );
+  }
+  
+  export default MainContent;
+  
